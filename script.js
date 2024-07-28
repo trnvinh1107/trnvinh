87921bb31e69f2be2d5108a88c17afb9cf9748a2
trnvinh1107 <142041420+trnvinh1107@users.noreply.github.com>
2024-07-28 16:36:37 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ const toggle = document.querySelector(".toggle__theme");
 const card = document.querySelector(".app");
 toggle.addEventListener("click", () => {
   let theme = toggle.querySelector(".fas");
-  if (theme.classList.contains("fa-moon")) {
-    theme.classList.remove("fa-moon");
-    theme.classList.add("fa-sun");
-    card.classList.add("dark");
-  } else {
+  if (theme.classList.contains("fa-sun")) {
     theme.classList.remove("fa-sun");
     theme.classList.add("fa-moon");
     card.classList.remove("dark");
+  } else {
+    theme.classList.remove("fa-moon");
+    theme.classList.add("fa-sun");
+    card.classList.add("dark");
   }
 });
 
